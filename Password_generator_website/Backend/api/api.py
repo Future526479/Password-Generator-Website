ffa7f28b4c87318ffdcceab5fa92a58eef1a2a73
Brian <kabelomadute19@gmail.com>
2026-03-14 15:19:45 +0200

    Update api.py

--- Password_generator_website/Backend/api/api.py
+++ Password_generator_website/Backend/api/api.py
@@ -1,1 +1,26 @@
+from fastapi import FastAPI
+from generater.generater_pass import password_generater
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
+api = FastAPI()
+
+origins = [
+    "http://localhost:5173"
+]
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Password_Length(BaseModel):
+    length: int
+
+@api.post("/generater")
+def password(data: Password_Length):
+    password = password_generater(data.length)
+    return {"password": password}
